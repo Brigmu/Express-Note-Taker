4d96d593a89c84bd55a2c30a8738451074f7200e
Brigham Mueller <brigmu@gmail.com>
2020-02-29 19:51:05 -0800

    added functionality for delete, added ids to objects to allow for delets to happen

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.json());
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
-    // res.sendFile(path.join(__dirname, "public/assets/css/styles.css"));
-    // res.sendFile(path.join(__dirname, "public/assets/js/index.js"));
   });
 
 app.get("/notes", function(req, res) {
@@ -22,7 +20,7 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    fs.readFileSync("db/db.json", "utf-8", (err, data) => {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
         if(err){
             throw err;
         }
@@ -40,18 +38,38 @@ app.post("/api/notes", function(req, res){
             throw err;
         };
         const content = JSON.parse(data);
+        newNote.id = content.length + 1;
         content.push(newNote);
         fs.writeFile("db/db.json", JSON.stringify(content, null, `\t`), "utf-8", (err) =>{
             if(err) {
                 throw err;
             }
-            console.log('wrote file');
             
         });
-    });
+    });   
 
-    
+})
 
+app.delete("/api/notes/:id", function(req, res){
+    const id = req.params.id;
+
+    res.json({response: 'ok'});
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+        if(err){
+            throw err;
+        };
+        const content = JSON.parse(data);
+        content.splice((id -1), 1);
+        for(let i = 0; i < content.length;i++){
+            content[i].id = i + 1;
+        }
+        fs.writeFile("db/db.json", JSON.stringify(content, null, `\t`), "utf-8", (err) =>{
+            if(err) {
+                throw err;
+            }
+            
+        });
+    }); 
 })
 
 app.listen(port, () =>{
